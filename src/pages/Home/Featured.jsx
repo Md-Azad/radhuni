3ec378a20174a422d9ef2fa200f8d84ff89ec851
Md Azad Hossain <sokalazad@gmail.com>
2025-01-02 20:14:22 +0100

    featured section updated

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -1,25 +1,52 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import "./featured.css";
+import featuredImg from "../../assets/home/featured.jpg";
 const Featured = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const d = new Date();
+
   return (
-    <div className="hero min-h-screen featured">
-      <div className="hero-overlay bg-opacity-60"></div>
+    <section className="featured bg-fixed  text-white pt-8 my-12">
       <SectionTitle
-        heading={"From our menu"}
-        subHeading={"check it out"}
+        heading="From our menu"
+        subHeading="check it out"
       ></SectionTitle>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36 bg-slate-500 bg-opacity-50">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div className="md:ml-10">
+          <h3>{`${
+            months[d.getMonth()]
+          } ${d.getDate()}, ${d.getFullYear()}`}</h3>
+          <h2 className="uppercase">Where can i get some?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+            asperiores illo repellat dolores neque aperiam sunt quasi in
+            adipisci rerum commodi totam rem, quaerat ad illum possimus sint
+            praesentium ipsa. Quod rem incidunt error consequatur soluta id
+            assumenda rerum nesciunt reiciendis ea, distinctio illum, labore
+            iure ab perspiciatis qui voluptate.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-outline mt-6 border-0 border-b-4 text-white">
+            Order Now
+          </button>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
