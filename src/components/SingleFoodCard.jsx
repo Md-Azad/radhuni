00f58f99e1b83ccc7e279a7912e7f7835f4f84c5
Md Azad Hossain <sokalazad@gmail.com>
2025-01-04 21:50:48 +0100

    added price and placed in corner of the image

--- src/components/SingleFoodCard.jsx
+++ src/components/SingleFoodCard.jsx
@@ -1,14 +1,17 @@
 const SingleFoodCard = ({ item }) => {
   const { name, image, recipe, price } = item;
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl relative">
       <figure>
         <img src={image} alt="food" />
       </figure>
       <div className="card-body">
+        <p className="bg-black text-white w-1/5 text-center rounded-lg absolute top-7 right-5">
+          ${price}
+        </p>
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-center">
           <button className="btn btn-primary">Add To Cart</button>
         </div>
       </div>
